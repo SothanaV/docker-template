e82db8e282c807a3dc2b84cfd655dbdd61bc28d2
SothanaV <nai6320@gmail.com>
2022-07-11 17:56:31 +0700

    [UPDATE] django template

--- django/start-django.sh
+++ django/start-django.sh
@@ -4,5 +4,6 @@ source env/bin/activate
 pip install django psycopg2-binary djangorestframework django-cors-headers
 django-admin startproject backend
 cd backend
+mkdir static
 pip freeze > requirements.txt
 cd ..

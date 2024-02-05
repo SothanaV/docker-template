67a7e4fe1403a4718d272147329d564cc20338c9
SothanaV <nai6320@gmail.com>
2024-02-05 15:45:06 +0700

    [UPDATE] django docker template

--- django/backend/runserver.sh
+++ django/backend/runserver.sh
@@ -2,4 +2,6 @@ while ! nc -w 1 -z ${DB_HOST} ${DB_PORT};
 do sleep 5;
 done;
 
-python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic --no-input
+
+gunicorn --workers=4 --timeout 300 --log-level debug --bind 0.0.0.0:8000 backend.wsgi

69810f9825ce5ff82d418dc51f838aa984b7d3a2
SothanaV <nai6320@gmail.com>
2024-02-14 10:18:02 +0700

    [UPDATE] gunicorn & psycopg2-binary

--- django/start-django.sh
+++ django/start-django.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 virtualenv env
 source env/bin/activate
-pip install django psycopg2-binary djangorestframework django-cors-headers
+pip install django psycopg2-binary djangorestframework django-cors-headers gunicorn
 django-admin startproject backend
 cd backend
 mkdir static

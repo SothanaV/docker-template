0b6a022d58d5a043f8ecf9ed8d30045ba48cf97a
SothanaV <nai6320@gmail.com>
2024-05-02 12:08:24 +0700

    [UPDATE] serve backend django via gunicorn

--- django/backend/runserver.sh
+++ django/backend/runserver.sh
@@ -4,4 +4,6 @@ done;
 
 python manage.py collectstatic --no-input
 
-gunicorn --workers=4 --timeout 300 --log-level debug --bind 0.0.0.0:8000 backend.wsgi
+n_worker=${1:-4}
+echo n_worker=$n_worker
+gunicorn --workers=$n_worker --timeout 300 --log-level debug --bind 0.0.0.0:8000 backend.wsgi

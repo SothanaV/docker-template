a46f5bd0ce094a2860a28261ecdff43e0f449afe
SothanaV <nai6320@gmail.com>
2024-07-10 17:07:46 +0700

    [UPDATE] fastapi runserver

--- fastapi/backend/run-server.sh
+++ fastapi/backend/run-server.sh
@@ -1,3 +1,3 @@
 n_worker=${1:-4}
 echo n_worker=$n_worker
-uvicorn --workers=$n_worker --log-level debug --bind 0.0.0.0:5000 server:app
+uvicorn --workers=$n_worker --log-level debug --host 0.0.0.0 --port 5000 server:app

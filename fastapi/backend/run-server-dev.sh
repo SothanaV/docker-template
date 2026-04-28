1ea1ce9c3ac0f065012bdf7c0de99d71fed73f86
SothanaV <nai6320@gmail.com>
2026-04-28 17:47:07 +0700

    [UPDATE] logs format and root path

--- fastapi/backend/run-server-dev.sh
+++ fastapi/backend/run-server-dev.sh
@@ -1,1 +1,2 @@
-uvicorn --reload --log-level debug --host 0.0.0.0 --port 5000 server:app
+root_path=${1:-}
+uvicorn --log-config log-config.yaml --reload --log-level debug --host 0.0.0.0 --port 5000 server:app --root-path "/${root_path}"
--- fastapi/backend/run-server.sh
+++ fastapi/backend/run-server.sh
@@ -1,3 +1,5 @@
 n_worker=${1:-4}
+root_path=${2:-}
 echo n_worker=$n_worker
-uvicorn --workers=$n_worker --log-level debug --host 0.0.0.0 --port 5000 server:app
+echo root_path=$root_path
+uvicorn --log-config log-config.yaml --workers=$n_worker --log-level debug --host 0.0.0.0 --port 5000 server:app --root-path "/${root_path}"
